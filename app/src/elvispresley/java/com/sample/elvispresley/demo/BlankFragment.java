95530aebf4b98dedb711f04b3d89dd0641ff2428
zhewuzhou <zhewuzhou@gmail.com>
2020-09-04 10:02:23 +0800

    [Z.Z][Android] Dependency & new model for elvispresley flavor

---
+++ app/src/elvispresley/java/com/sample/elvispresley/demo/BlankFragment.java
@@ -0,0 +1,34 @@
+package com.sample.elvispresley.demo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import com.example.myapplication.R;
+
+public class BlankFragment extends Fragment {
+
+    private BlankViewModel mViewModel;
+
+    public static BlankFragment newInstance() {
+        return new BlankFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.blank_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = ViewModelProviders.of(this).get(BlankViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/elvispresley/java/com/sample/elvispresley/demo/BlankViewModel.java
@@ -0,0 +1,7 @@
+package com.sample.elvispresley.demo;
+
+import androidx.lifecycle.ViewModel;
+
+public class BlankViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
